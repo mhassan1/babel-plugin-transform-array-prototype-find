50830789bb6f64f23c8db0add0f533d729e6c760
mhassan1 <marc.j.hassan@gmail.com>
2025-06-10 11:23:28 -0400

    Add test for `thisArg`

--- src/__tests__/plugin.test.ts
+++ src/__tests__/plugin.test.ts
@@ -38,6 +38,10 @@ pluginTester({
       output: "Array.isArray('a') ? 'a'.filter((v) => v === 1)[0] : 'a'['find']((v) => v === 1);",
     },
     {
+      code: 'a.find(v => v === 1, t);',
+      output: 'Array.isArray(a) ? a.filter((v) => v === 1, t)[0] : a.find((v) => v === 1, t);',
+    },
+    {
       code: 'class a extends b {\n  constructor() {\n    super.find((v) => v === 1);\n  }\n}',
       output:
         'class a extends b {\n  constructor() {\n    Array.isArray(this) ? super.filter((v) => v === 1)[0] : super.find((v) => v === 1);\n  }\n}',
